5b02bebf21d62a2082a6da5b5cd98568677ab1fb
ChooChooShoe <timothy.durbano@gmail.com>
2016-03-24 23:00:54 -0400

    New Icon system, many fixes, saves files, and more.

---
+++ Win10TileEditor.Core/ShellHelper.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Win10TileEditor.Core
+{
+    public static class ShellHelper
+    {
+        [DllImport("shell32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern bool SHObjectProperties(IntPtr hwnd, uint shopObjectType, string pszObjectName, string pszPropertyPage);
+        
+        public static uint SHOP_PRINTERNAME = 0x1;  // lpObject points to a printer friendly name
+        public static uint SHOP_FILEPATH = 0x2;  // lpObject points to a fully qualified path+file name
+        public static uint SHOP_VOLUMEGUID = 0x4;  // lpObject points to a Volume GUID
+
+        public static bool ShowPropertiesDialog(IntPtr handle, string filename)
+        {
+            try
+            {
+                return SHObjectProperties(handle, SHOP_FILEPATH, filename, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        public static dynamic createWshShell()
+        {
+            Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8")); //Windows Script Host Shell Object
+            return Activator.CreateInstance(t);
+        }
+
+        /*
+        public static void ReadFilesProperties(object sender)
+        {
+            dynamic shell = ShellHelper.createWshShell();
+            try
+            {
+                while (_linksToReadStack.Count > 0)
+                {
+                    LinkViewItem item = _linksToReadStack.Pop();// LinkViewItem
+                    WshShortcut lnk = new WshShortcut(shell.CreateShortcut(item.File.FullName));
+                    try
+                    {
+
+                        item.IconLocation = lnk.IconLocation;
+                        item.Description = lnk.Description;
+                        item.TargetPath = lnk.TargetPath;
+
+                        Console.WriteLine("Icon: {0}", lnk.IconLocation);
+
+
+
+                        if (item.TargetPath != null && item.TargetPath.Length > 3)
+                        {
+                            //icon
+                            IconExtractor ex = new IconExtractor(item.TargetPath);
+
+                            //Console.WriteLine("Icon Extract: cout={0} FIleName={1} toString={2}",ex.Count,ex.FileName, ex.ToString());
+                            item.Icon = new Icon(ex.GetIconAt(0), 16, 16).ToBitmap();
+
+                            ex.Dispose();
+                            //int index = 0;
+                            //StringBuilder strB = new StringBuilder(item.TargetPath);
+                            //IntPtr handle = SafeNativeMethods.ExtractAssociatedIcon((IntPtr)e.Argument, strB, ref index);
+                            //Icon ico = Icon.FromHandle(handle);
+                            //item.Icon = ico.ToBitmap();// new Bitmap(ico.ToBitmap(), new Size(16, 16));
+
+                            //Master.imageList1.Images.Add(ico);
+                            //Master.debugPictureBox.Image = Master.imageList1.Images[0];
+
+                            //Shell32.Shell shell = new Shell32.Shell();
+                            //shell.MinimizeAll();
+
+                            //icon
+
+                            var file = new FileInfo(item.TargetPath.Substring(0, item.TargetPath.Length - 3) + "VisualElementsManifest.xml");
+                            item.ManifestData = new VisualManifest(file);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("And error has occured: '{0}'", ex);
+                    }
+                    finally
+                    {
+                        Marshal.FinalReleaseComObject(lnk);
+                    }
+                    //_worker.ReportProgress(0, item);
+                }
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(shell);
+            }
+
+        }*/
+
+        public static ImageSource GetIcon(string path, bool smallIcon, bool isDirectory)
+        {
+            // SHGFI_USEFILEATTRIBUTES takes the file name and attributes into account if it doesn't exist
+            uint flags = SHGFI_ICON | SHGFI_USEFILEATTRIBUTES;
+            if (smallIcon)
+                flags |= SHGFI_SMALLICON;
+
+            uint attributes = FILE_ATTRIBUTE_NORMAL;
+            if (isDirectory)
+                attributes |= FILE_ATTRIBUTE_DIRECTORY;
+
+            SHFILEINFO shfi;
+            if (0 != SHGetFileInfo(path, attributes, out shfi, (uint)Marshal.SizeOf(typeof(SHFILEINFO)), flags))
+            {
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(shfi.hIcon, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            return null;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        }
+
+        [DllImport("shell32")]
+        private static extern int SHGetFileInfo(string pszPath, uint dwFileAttributes, out SHFILEINFO psfi, uint cbFileInfo, uint flags);
+
+        private const uint FILE_ATTRIBUTE_READONLY = 0x00000001;
+        private const uint FILE_ATTRIBUTE_HIDDEN = 0x00000002;
+        private const uint FILE_ATTRIBUTE_SYSTEM = 0x00000004;
+        private const uint FILE_ATTRIBUTE_DIRECTORY = 0x00000010;
+        private const uint FILE_ATTRIBUTE_ARCHIVE = 0x00000020;
+        private const uint FILE_ATTRIBUTE_DEVICE = 0x00000040;
+        private const uint FILE_ATTRIBUTE_NORMAL = 0x00000080;
+        private const uint FILE_ATTRIBUTE_TEMPORARY = 0x00000100;
+        private const uint FILE_ATTRIBUTE_SPARSE_FILE = 0x00000200;
+        private const uint FILE_ATTRIBUTE_REPARSE_POINT = 0x00000400;
+        private const uint FILE_ATTRIBUTE_COMPRESSED = 0x00000800;
+        private const uint FILE_ATTRIBUTE_OFFLINE = 0x00001000;
+        private const uint FILE_ATTRIBUTE_NOT_CONTENT_INDEXED = 0x00002000;
+        private const uint FILE_ATTRIBUTE_ENCRYPTED = 0x00004000;
+        private const uint FILE_ATTRIBUTE_VIRTUAL = 0x00010000;
+
+        private const uint SHGFI_ICON = 0x000000100;     // get icon
+        private const uint SHGFI_DISPLAYNAME = 0x000000200;     // get display name
+        private const uint SHGFI_TYPENAME = 0x000000400;     // get type name
+        private const uint SHGFI_ATTRIBUTES = 0x000000800;     // get attributes
+        private const uint SHGFI_ICONLOCATION = 0x000001000;     // get icon location
+        private const uint SHGFI_EXETYPE = 0x000002000;     // return exe type
+        private const uint SHGFI_SYSICONINDEX = 0x000004000;     // get system icon index
+        private const uint SHGFI_LINKOVERLAY = 0x000008000;     // put a link overlay on icon
+        private const uint SHGFI_SELECTED = 0x000010000;     // show icon in selected state
+        private const uint SHGFI_ATTR_SPECIFIED = 0x000020000;     // get only specified attributes
+        private const uint SHGFI_LARGEICON = 0x000000000;     // get large icon
+        private const uint SHGFI_SMALLICON = 0x000000001;     // get small icon
+        private const uint SHGFI_OPENICON = 0x000000002;     // get open icon
+        private const uint SHGFI_SHELLICONSIZE = 0x000000004;     // get shell size icon
+        private const uint SHGFI_PIDL = 0x000000008;     // pszPath is a pidl
+        private const uint SHGFI_USEFILEATTRIBUTES = 0x000000010;     // use passed dwFileAttribute
+    }
+}
--- Win10TileEditor.UI/MainWindow.xaml.cs
+++ Win10TileEditor.UI/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Win10TileEditor.Core;
+using Win10TileEditor.Dialog;
+using Win10TileEditor.Tree.ViewModels;
 
 namespace Win10TileEditor
 {
@@ -20,9 +16,173 @@ namespace Win10TileEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public BindableVisualManifest VisManifest { get { return Resources["VisualManifest"] as BindableVisualManifest; } }
+        public BindableShortcutData ShortcutData { get { return Resources["ShortcutData"] as BindableShortcutData; } }
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            this.Browser.Worker.ProgressChanged += worker_ProgressChanged;
+            this.Browser.Worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+
+            //var b = new BitmapImage();
+
+            //this.image0.Source = ShellHelper.GetIcon(@"‪C:\Games\UNDERTALE_v1.1.exe", true, false);
+        }
+
+        private void image150Browse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files|*.png;*.jpeg;*.jpg;*.gif|All files|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (openFileDialog.ShowDialog() == true)
+                this.image150Path.Text = openFileDialog.FileName;
+        }
+
+        private void image70Browse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files|*.png;*.jpeg;*.jpg;*.gif|All files|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (openFileDialog.ShowDialog() == true)
+                this.image70Path.Text = openFileDialog.FileName;
+        }
+
+        private void image150BrowseIcon_Click(object sender, RoutedEventArgs e)
+        {
+            PickIconDialog pickIconDialog = new PickIconDialog(this.ShortcutData.TargetPath, 0);
+
+            if (pickIconDialog.ShowDialog() == true)
+            {
+                this.image150Path.Text = pickIconDialog.Model.IconPath;
+                this.image150.DataContext = pickIconDialog.Model.SelectedIconSize;
+            }
+        }
+
+        private void image70BrowseIcon_Click(object sender, RoutedEventArgs e)
+        {
+            PickIconDialog pickIconDialog = new PickIconDialog(this.ShortcutData.TargetPath, 0);
+
+            if (pickIconDialog.ShowDialog() == true)
+            {
+                this.image70Path.Text = pickIconDialog.Model.IconPath;
+                this.image70.DataContext = pickIconDialog.Model.SelectedIconSize;
+
+            }
+        }
+
+        private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;// !String.IsNullOrWhiteSpace(TargetFile) && File.Exists(TargetFile) && VisManifest.IsValid;
+        }
+
+        
+        private void Save_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(ShortcutData.TargetPath) && File.Exists(ShortcutData.TargetPath) && VisManifest.IsValid)
+            {
+                FileInfo exeFile = new FileInfo(ShortcutData.TargetPath);
+
+                if (!exeFile.Exists)
+                {
+                    MessageBox.Show("No application found at '" + ShortcutData.TargetPath + "'", "Can't save changes");
+                    return;
+                }
+                var baseName = exeFile.Name.Substring(0, exeFile.Name.Length - exeFile.Extension.Length);
+
+                var visualPath = new FileInfo(exeFile.DirectoryName + "\\" + baseName + ".VisualElementsManifest.xml");
+
+                if (!String.IsNullOrEmpty(this.image150Path.Text))//TODO Make this optinal
+                {
+                    var fileIcon150 = baseName + "Icon150x150.png";
+                    var fileIcon70 = baseName + "Icon70x70.png";
+
+                    VisManifest.Square150x150Logo = fileIcon150;
+                    VisManifest.Square70x70Logo = fileIcon70;
+
+                    fileIcon150 = exeFile.DirectoryName + "\\" + fileIcon150;
+                    fileIcon70 = exeFile.DirectoryName + "\\" + fileIcon70;
+
+                    //TODO save exception
+                    using (var fileStream = new FileStream(fileIcon150, FileMode.Create))
+                    {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(this.image150.Source as BitmapSource));
+                        encoder.Save(fileStream);
+                    }
+                    using (var fileStream = new FileStream(fileIcon70, FileMode.Create))
+                    {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(this.image70.Source as BitmapSource));
+                        encoder.Save(fileStream);
+                    }
+                    //this._image150FileName.CopyTo(fileIcon150);
+                    //this.image70FileName.CopyTo(fileIcon70);
+                }
+                if (VisManifest.saveToFile(visualPath.FullName))
+                {
+                    File.SetLastWriteTime(ShortcutData.LinkPath, DateTime.Now);
+                    //if (item.ManifestData != null)
+                    //    this.VisManifest.loadFromManifest(item.ManifestData);
+                }
+            }
+        }
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.statusProgress.Value = 0;
+            this.statusPath.Text = "Loading Link data completed.";
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            switch (e.ProgressPercentage)
+            {
+                case 97:
+                    dynamic[] data = e.UserState as object[];
+                    this.statusProgress.Value = data[0];
+                    Console.WriteLine(this.statusProgress.Value);
+                    this.statusPath.Text = data[1];
+                    break;
+            }
+        }
+
+        internal void openItem(SearchTreeItem oldItem, SearchTreeItem newItem)
+        {
+            LinkItem item = newItem as LinkItem;
+            if (item == null)
+                return;
+            if(item.ManifestData != null)
+            {
+                this.VisManifest.loadFromManifest(item.ManifestData);
+                this.image150.Source = null;
+                this.image70.Source = null;
+            }
+            if (item.ShortcutData != null)
+                this.ShortcutData.loadFromShortcut(item.ShortcutData);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.statusProgress.Value = 0;
+            this.Browser.StartWorker();
+        }
+
+        private void OpenFileProperties_Click(object sender, RoutedEventArgs e)
+        {
+            ShellHelper.ShowPropertiesDialog(IntPtr.Zero, this.ShortcutData.LinkPath);
         }
     }
 }
